c7d7bde85a6925a5c85646a5ba8d9ecf45929945
Biplav-05 <biplav413@gmail.com>
2024-10-18 11:15:17 +0000

    chore(release): 0.0.1-rc.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+
+
+## 0.0.1-rc.0 (2024-10-18)
+
+
+### Bug Fixes
+
+* oh no ([2e25301](https://github.com/Biplav-05/python_sdk_test/commit/2e253011f346c64af8a0aeb06d48db45107e94a8))
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="habibi",
-    version="0.0.0",
+    version="0.0.1-rc.0",
     packages=find_packages(),
     install_requires=[],
     author="Biplav",
