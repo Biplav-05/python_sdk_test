a10d7bc0f06db95d332d9a4df564ef349044e5dc
BIPLAV00 <biplav.ghimire@truenary.com>
2024-10-18 14:10:18 +0545

    feat: hello

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="biplav_test",
-    version="0.1.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[],
     author="Biplav",
